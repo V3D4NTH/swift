94464933692847be02d99ac01371243cfa8ee302
dartixus <dschnurp@students.zcu.cz>
2022-11-06 11:27:43 +0100

    extending sample input

--- sample_input/sample.swift
+++ sample_input/sample.swift
@@ -6,12 +6,18 @@ var a: Int = count(10)
 // print(count(number: a))
 if (a <= 5 || 9 == 9) {
     a = a + 1
+} else {
+    a -= 3
 }
 
-let A: Int = 100 var c: Int = 5
+let A: Int = 100; var c: Int = 5
 while (a > 1 && a < 1000) {
     a += 1
 }
+
+repeat {
+  a += 1
+} while A <= 100;
 
 
 
