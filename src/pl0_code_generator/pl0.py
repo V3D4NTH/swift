b4fb698a75a806572b55634e365c64f0ef7a13e5
dartixus <dschnurp@students.zcu.cz>
2022-11-11 19:13:55 +0100

    refactor

--- src/pl0_code_generator/pl0.py
+++ src/pl0_code_generator/pl0.py
@@ -2,30 +2,10 @@
 #  author: Daniel Schnurpfeil
 #
 
-from src.pl0_code_generator.const import Inst as t, Op as o
 from ete3 import Tree
 
-
-def inst(instruction: t):
-    """
-    It takes an instruction and returns its value
-
-    :param instruction: The instruction to be executed
-    :type instruction: t
-    :return: The value of the instruction.
-    """
-    return instruction.value
-
-
-def op(operation: o):
-    """
-    Return the value of the operation.
-
-    :param operation: The operation to perform
-    :type operation: o
-    :return: The value of the operation.
-    """
-    return operation.value
+from src.pl0_code_generator.const import Inst as t, Op as o
+from src.pl0_code_generator.pl0_utils import inst, op
 
 
 # > The class Pl0 is a class that represents a PL/0 program
