6f52b378cafacf795dfe6a266812c4187a62bc33
dartixus <dschnurp@students.zcu.cz>
2022-12-30 17:50:20 +0100

    bools and refactoring tests due to corrected bug (2h)

--- test/test_main.py
+++ test/test_main.py
@@ -74,7 +74,7 @@ class Test(TestCase):
 4 LIT 0 52
 5 LIT 0 43
 6 OPR 0 12
-7 JMC 0 13
+7 JMC 0 12
 8 LIT 0 32
 9 LOD 0 3
 10 OPR 0 4
@@ -91,21 +91,20 @@ class Test(TestCase):
 4 LOD 0 3
 5 LIT 0 5
 6 OPR 0 10
-7 JMC 0 21
+7 JMC 0 20
 8 LIT 0 52
 9 LIT 0 43
 10 OPR 0 12
-11 JMC 0 21
+11 JMC 0 20
 12 LOD 0 3
 13 LIT 0 5
 14 OPR 0 12
-15 JMC 0 21
+15 JMC 0 20
 16 LIT 0 32
 17 LOD 0 3
 18 OPR 0 4
 19 STO 0 3
-20 LIT 0 32
-21 RET 0 0
+20 RET 0 0
 """, code, "if_and")
 
     def test_if_and_or(self):
@@ -123,17 +122,16 @@ class Test(TestCase):
 10 LIT 0 52
 11 LIT 0 43
 12 OPR 0 12
-13 JMC 0 23
+13 JMC 0 22
 14 LOD 0 3
 15 LIT 0 5
 16 OPR 0 10
-17 JMC 0 23
+17 JMC 0 22
 18 LIT 0 32
 19 LOD 0 3
 20 OPR 0 4
 21 STO 0 3
-22 LIT 0 32
-23 RET 0 0
+22 RET 0 0
 """, code, "if_and_or")
 
     def test_if_or(self):
@@ -157,14 +155,50 @@ class Test(TestCase):
 16 LIT 0 1
 17 LIT 0 1
 18 OPR 0 12
-19 JMC 0 25
+19 JMC 0 24
 20 LIT 0 32
 21 LOD 0 3
 22 OPR 0 4
 23 STO 0 3
-24 LIT 0 32
-25 RET 0 0
+24 RET 0 0
 """, code, "if_or")
+
+    def test_bool(self):
+        code = start_compiler("../sample_input/bool.swift")
+        self.assertEqual("""0 INT 0 3
+1 INT 0 1
+2 LIT 0 1
+3 STO 0 3
+4 INT 0 1
+5 LIT 0 0
+6 STO 0 4
+7 LOD 0 4
+8 JMC 0 12
+9 INT 0 1
+10 LIT 0 9000
+11 STO 0 5
+12 LOD 0 3
+13 JMC 0 17
+14 INT 0 1
+15 LIT 0 1000
+16 STO 0 6
+17 RET 0 0
+""", code, "bool")
+
+    def test_complex_bool(self):
+            code = start_compiler("../sample_input/complex_bool.swift")
+            self.assertEqual("""0 INT 0 3
+1 LIT 0 1
+2 JMC 0 10
+3 LIT 0 1
+4 LIT 0 0
+5 OPR 0 12
+6 JMC 0 10
+7 INT 0 1
+8 LIT 0 9000
+9 STO 0 3
+10 RET 0 0
+""", code, "complex_bool")
 
     def test_if_else(self):
         code = start_compiler("../sample_input/if_else.swift")
@@ -201,7 +235,7 @@ class Test(TestCase):
 8 LIT 0 100
 9 LIT 0 43
 10 OPR 0 10
-11 JMC 0 17
+11 JMC 0 16
 12 LIT 0 32
 13 LOD 0 3
 14 OPR 0 4
@@ -296,7 +330,8 @@ class Test(TestCase):
 17 OPR 0 2
 18 STO 0 4
 19 JMP 0 7
-20 RET 0 0
+20 LIT 0 1
+21 RET 0 0
 """, code, "for")
 
     def test_func(self):
@@ -395,7 +430,7 @@ class Test(TestCase):
 1 INT 0 1
 2 LIT 0 52
 3 STO 0 3
-4 JMP 0 26
+4 JMP 0 27
 5 INT 0 3
 6 LOD 0 -1
 7 INT 0 1
@@ -414,21 +449,22 @@ class Test(TestCase):
 20 OPR 0 2
 21 STO 0 4
 22 JMP 0 10
-23 LOD 0 3
-24 STO 0 -2
-25 RET 0 0
-26 INT 0 1
-27 LOD 0 3
-28 CAL 0 5
-29 INT 0 -1
-30 STO 0 3
-31 RET 0 0
+23 LIT 0 1
+24 LOD 0 3
+25 STO 0 -2
+26 RET 0 0
+27 INT 0 1
+28 LOD 0 3
+29 CAL 0 5
+30 INT 0 -1
+31 STO 0 3
+32 RET 0 0
 """, code, "for_in_func")
 
     def test_program(self):
         code = start_compiler("../sample_input/program.swift")
         self.assertEqual("""0 INT 0 3
-1 JMP 0 28
+1 JMP 0 27
 2 INT 0 3
 3 LOD 0 -1
 4 INT 0 1
@@ -452,55 +488,55 @@ class Test(TestCase):
 22 LOD 0 4
 23 OPR 0 4
 24 STO 0 4
-25 LOD 0 4
-26 STO 0 -2
-27 RET 0 0
-28 JMP 0 50
-29 INT 0 3
-30 LOD 0 -1
-31 INT 0 1
-32 LIT 0 1
-33 STO 0 4
-34 LOD 0 4
-35 LIT 0 21
-36 OPR 0 10
-37 JMC 0 47
-38 LOD 0 4
-39 LIT 0 5
-40 OPR 0 2
-41 STO 0 3
-42 LIT 0 1
-43 LOD 0 4
-44 OPR 0 2
-45 STO 0 4
-46 JMP 0 34
-47 LOD 0 3
-48 STO 0 -2
-49 RET 0 0
-50 JMP 0 77
-51 INT 0 3
-52 LOD 0 -2
-53 LOD 0 -1
-54 INT 0 1
-55 LIT 0 1
-56 STO 0 5
-57 LOD 0 5
-58 LIT 0 20
-59 OPR 0 10
-60 JMC 0 74
-61 LIT 0 1
-62 LOD 0 3
-63 OPR 0 2
-64 STO 0 3
-65 LIT 0 2
-66 LOD 0 4
-67 OPR 0 2
-68 STO 0 4
-69 LIT 0 1
-70 LOD 0 5
-71 OPR 0 2
-72 STO 0 5
-73 JMP 0 57
+25 STO 0 -2
+26 RET 0 0
+27 JMP 0 49
+28 INT 0 3
+29 LOD 0 -1
+30 INT 0 1
+31 LIT 0 1
+32 STO 0 4
+33 LOD 0 4
+34 LIT 0 21
+35 OPR 0 10
+36 JMC 0 46
+37 LOD 0 4
+38 LIT 0 5
+39 OPR 0 2
+40 STO 0 3
+41 LIT 0 1
+42 LOD 0 4
+43 OPR 0 2
+44 STO 0 4
+45 JMP 0 33
+46 LOD 0 3
+47 STO 0 -2
+48 RET 0 0
+49 JMP 0 77
+50 INT 0 3
+51 LOD 0 -2
+52 LOD 0 -1
+53 INT 0 1
+54 LIT 0 1
+55 STO 0 5
+56 LOD 0 5
+57 LIT 0 20
+58 OPR 0 10
+59 JMC 0 73
+60 LIT 0 1
+61 LOD 0 3
+62 OPR 0 2
+63 STO 0 3
+64 LIT 0 2
+65 LOD 0 4
+66 OPR 0 2
+67 STO 0 4
+68 LIT 0 1
+69 LOD 0 5
+70 OPR 0 2
+71 STO 0 5
+72 JMP 0 56
+73 LIT 0 2
 74 LOD 0 3
 75 STO 0 -3
 76 RET 0 0
@@ -568,7 +604,7 @@ class Test(TestCase):
 138 INT 0 1
 139 LOD 0 7
 140 LOD 0 8
-141 CAL 0 51
+141 CAL 0 50
 142 INT 0 -2
 143 STO 0 7
 144 INT 0 1
@@ -593,7 +629,7 @@ class Test(TestCase):
 1 INT 0 1
 2 LIT 0 52
 3 STO 0 3
-4 JMP 0 26
+4 JMP 0 27
 5 INT 0 3
 6 LOD 0 -1
 7 INT 0 1
@@ -612,47 +648,48 @@ class Test(TestCase):
 20 OPR 0 2
 21 STO 0 4
 22 JMP 0 10
-23 LOD 0 3
-24 STO 0 -2
-25 RET 0 0
-26 INT 0 1
-27 LOD 0 3
-28 CAL 0 5
-29 INT 0 -1
-30 STO 0 3
-31 LIT 0 100
-32 LOD 0 3
-33 OPR 0 10
-34 JMC 0 45
-35 LOD 0 3
-36 LIT 0 1
-37 OPR 0 3
-38 STO 0 3
-39 LOD 0 3
-40 LIT 0 50
-41 OPR 0 12
-42 JMC 0 44
-43 JMP 0 35
-44 JMP 0 65
-45 LIT 0 1
-46 LOD 0 3
-47 OPR 0 10
-48 JMC 0 66
-49 LIT 0 32
-50 LOD 0 3
-51 OPR 0 4
-52 STO 0 3
-53 LOD 0 3
-54 LIT 0 43
-55 OPR 0 12
-56 JMC 0 61
-57 LOD 0 3
-58 LIT 0 9
-59 OPR 0 2
-60 JMP 0 64
-61 LOD 0 3
-62 LIT 0 5
-63 OPR 0 2
-64 STO 0 3
-65 RET 0 0
+23 LIT 0 1
+24 LOD 0 3
+25 STO 0 -2
+26 RET 0 0
+27 INT 0 1
+28 LOD 0 3
+29 CAL 0 5
+30 INT 0 -1
+31 STO 0 3
+32 LIT 0 100
+33 LOD 0 3
+34 OPR 0 10
+35 JMC 0 46
+36 LOD 0 3
+37 LIT 0 1
+38 OPR 0 3
+39 STO 0 3
+40 LOD 0 3
+41 LIT 0 50
+42 OPR 0 12
+43 JMC 0 45
+44 JMP 0 36
+45 JMP 0 66
+46 LIT 0 1
+47 LOD 0 3
+48 OPR 0 10
+49 JMC 0 66
+50 LIT 0 32
+51 LOD 0 3
+52 OPR 0 4
+53 STO 0 3
+54 LOD 0 3
+55 LIT 0 43
+56 OPR 0 12
+57 JMC 0 62
+58 LOD 0 3
+59 LIT 0 9
+60 OPR 0 2
+61 JMP 0 65
+62 LOD 0 3
+63 LIT 0 5
+64 OPR 0 2
+65 STO 0 3
+66 RET 0 0
 """, code, "complex_program")
