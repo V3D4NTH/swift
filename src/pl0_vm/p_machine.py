2b033f748f9c477e3a2a0bff7ce092c12d894163
dartixus <dschnurp@students.zcu.cz>
2022-12-31 14:44:41 +0100

    python doc

--- src/pl0_vm/p_machine.py
+++ src/pl0_vm/p_machine.py
@@ -6,7 +6,13 @@ from copy import copy
 from src.pl0_code_generator.instructions import Inst, Op
 
 
-def ret_stck_str(stack: list) -> str:
+def ret_stack_as_str(stack: list) -> str:
+    """
+    It returns a string representation of the stack.
+
+    :param stack: list
+    :type stack: list
+    """
     ret_val = ""
     for index, i in enumerate(stack):
         ret_val += str(index) + "\t" + str(i) + "\n"
@@ -14,6 +20,12 @@ def ret_stck_str(stack: list) -> str:
 
 
 def run_pl0_code(generated_code: list) -> str:
+    """
+    It takes a list of pl/0 code, and returns a string of stack
+
+    :param generated_code: list
+    :type generated_code: list
+    """
     if len(generated_code) > 130:
         return "code is too long"
     static_base = 0
@@ -21,7 +33,9 @@ def run_pl0_code(generated_code: list) -> str:
     instruction_pointer = 0
     stack = []
 
+    # Checking if the instruction pointer is less than the length of the generated code.
     while instruction_pointer < len(generated_code):
+        # Checking if the instruction is a lit.
         if generated_code[instruction_pointer][0] == Inst.lit.value:
             stack_pointer += 1
             if stack_pointer >= len(stack):
@@ -29,6 +43,7 @@ def run_pl0_code(generated_code: list) -> str:
             else:
                 stack[stack_pointer] = generated_code[instruction_pointer][2]
 
+        # Checking if the instruction is an operator.
         elif generated_code[instruction_pointer][0] == Inst.opr.value:
 
             if int(generated_code[instruction_pointer][2]) == Op.neg.value:
@@ -95,16 +110,19 @@ def run_pl0_code(generated_code: list) -> str:
                     stack[stack_pointer - 1] = 0
                 stack_pointer -= 1
 
+        # It loads the value of the stack to the stack pointer.
         elif generated_code[instruction_pointer][0] == Inst.lod.value:
             stack_pointer += 1
             if stack_pointer - len(stack) > -1:
                 stack.append(0)
             stack[stack_pointer] = stack[generated_code[instruction_pointer][2] + static_base]
 
+        # It stores the value of the stack pointer to the stack.
         elif generated_code[instruction_pointer][0] == Inst.sto.value:
             stack[generated_code[instruction_pointer][2] + static_base] = stack[stack_pointer]
             stack_pointer -= 1
 
+        # Calling a function.
         elif generated_code[instruction_pointer][0] == Inst.cal.value:
             stack.append(stack_pointer)
             stack.append(0)
@@ -112,12 +130,15 @@ def run_pl0_code(generated_code: list) -> str:
             instruction_pointer = generated_code[instruction_pointer][2] - 1
             static_base = stack_pointer + 1
 
+        # It adds the value of the instruction to the stack pointer.
+        # If the stack pointer is greater than the length of the stack, it adds 0 to the stack.
         elif generated_code[instruction_pointer][0] == Inst.int.value:
             stack_pointer += generated_code[instruction_pointer][2]
             if stack_pointer - len(stack) > -1:
                 for _ in range(stack_pointer - len(stack) + 1):
                     stack.append(0)
 
+        # Returning the value of the stack.
         elif generated_code[instruction_pointer][0] == Inst.ret.value:
             if static_base == 0:
                 break
@@ -125,16 +146,20 @@ def run_pl0_code(generated_code: list) -> str:
             stack_pointer = static_base - 1
             static_base = 0
 
+        # Checking if the stack pointer is 0. If it is, it jumps to the instruction that is given in the generated code.
         elif generated_code[instruction_pointer][0] == Inst.jmc.value:
             if stack[stack_pointer] == 0:
                 instruction_pointer = generated_code[instruction_pointer][2] - 1
 
+        # Jumping to the instruction that is given in the generated code.
         elif generated_code[instruction_pointer][0] == Inst.jmp.value:
             instruction_pointer = generated_code[instruction_pointer][2] - 1
 
+        # Checking if the instruction pointer is less than the length of the generated code and stack controls too.
         if instruction_pointer < 0 or instruction_pointer > len(generated_code) or \
                 stack_pointer < 0 or stack_pointer > len(stack):
             raise IndexError("ERR in executing generated code...")
 
+        # Incrementing the instruction pointer by 1.
         instruction_pointer += 1
-    return ret_stck_str(stack)
+    return ret_stack_as_str(stack)
